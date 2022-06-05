68e0a776e6765a545bce23158fcb818e31f899e4
sashauk <sautksnsashaa@gmail.com>
2022-06-06 01:19:06 +0300

    Добавьте файлы проекта.

---
+++ CalcServer/CalcService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CalcInterfaces;
+
+namespace CalcServer
+{
+    internal class CalcService : ICalcService
+    {
+        public int Sum(int firstNumber, int secondNumber)
+        {
+            return firstNumber + secondNumber;
+        }
+    }
+}
